ecd2b097e0e338a17366a733fb5759e556a9537a
MuhammadHassan0184 <muhammadhassanofficial0184@gmail.com>
2026-01-19 18:19:12 +0500

    splashupdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,3 @@
-// import 'package:birthdaycounter/config/Routes/routes.dart';
-// import 'package:birthdaycounter/config/Routes/routes_name.dart';
-// import 'package:flutter/material.dart';
-// import 'package:get/get_navigation/src/root/get_material_app.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return GetMaterialApp(
-//       debugShowCheckedModeBanner: false,
-
-//       // home: SplashScreen(),
-//       getPages: AppRoutes.routes,
-//       initialRoute: AppRoutesName.onboardingSlider,
-
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//       ),
-//       // getPages: AppRoutes.routes(),
-//     );
-//   }
-// }
-
 import 'package:birthdaycounter/config/Routes/routes.dart';
 import 'package:birthdaycounter/config/Routes/routes_name.dart';
 import 'package:birthdaycounter/view/splash/splash_screen.dart';
--- lib/view/splash/splash_screen.dart
+++ lib/view/splash/splash_screen.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: sized_box_for_whitespace
+
+import 'package:birthdaycounter/config/Colors/colors.dart';
 import 'package:birthdaycounter/config/Routes/routes_name.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -23,9 +26,49 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset(
-          "assets/Splashimg.png", // your splash image
-          fit: BoxFit.contain,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 180,
+              height: 216,
+              child: Image.asset(
+                "assets/Splashimg.png", // your splash image
+                fit: BoxFit.contain,
+              ),
+            ),
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Birthday ",
+                    style: TextStyle(
+                      color: AppColors.primary,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "& Events\n",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text: "Reminder",
+                    style: TextStyle(
+                      color: AppColors.primary,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
