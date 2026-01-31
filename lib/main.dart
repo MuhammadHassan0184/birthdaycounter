9a0f5dba0a90bd294d3ceb07da4a82687538c1b0
MuhammadHassan0184 <muhammadhassanofficial0184@gmail.com>
2026-01-31 18:40:37 +0500

    complete1

--- lib/main.dart
+++ lib/main.dart
@@ -1,99 +1,17 @@
-// // ignore_for_file: avoid_print
-
-// import 'package:birthdaycounter/Services/notification_service.dart';
-// import 'package:birthdaycounter/config/Routes/routes_name.dart';
-// import 'package:birthdaycounter/view/splash/splash_screen.dart';
-// import 'package:birthdaycounter/config/Routes/routes.dart';
-// import 'package:get/get_navigation/get_navigation.dart';
-// import 'package:birthdaycounter/firebase_options.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:get/get_core/src/get_main.dart';
-// import 'package:flutter/material.dart';
-
-// final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   print("Starting Firebase init...");
-
-//   try {
-//     await Firebase.initializeApp(
-//       options: DefaultFirebaseOptions.currentPlatform,
-//     );
-//     print("Firebase initialized SUCCESSFULLY!");
-//   } catch (e) {
-//     print("Firebase Init Error: $e");
-//   }
-//   await NotificationService.init(); // 🔥 REQUIRED
-
-//   // Set default Firebase Auth language
-//   FirebaseAuth.instance.setLanguageCode('en');
-
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return GetMaterialApp(
-//       navigatorKey: navigatorKey, // 👈 REQUIRED
-
-//       debugShowCheckedModeBanner: false,
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         fontFamily: 'Poppins',
-//         textTheme: GoogleFonts.poppinsTextTheme(),
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//       ),
-//       home: const SplashWrapper(), // show splash first
-//       getPages: AppRoutes.routes,
-//     );
-//   }
-// }
-
-// // A wrapper to handle splash delay
-// class SplashWrapper extends StatefulWidget {
-//   const SplashWrapper({super.key});
-
-//   @override
-//   State<SplashWrapper> createState() => _SplashWrapperState();
-// }
-
-// class _SplashWrapperState extends State<SplashWrapper> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Delay 3 seconds then go to onboarding
-//     Future.delayed(const Duration(seconds: 3), () {
-//       Get.offNamed(AppRoutesName.onboardingSlider); // navigate after splash
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const SplashScreen();
-//   }
-// }
-
 // ignore_for_file: avoid_print
 
 import 'dart:convert';
-import 'package:birthdaycounter/Services/notification_service.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:birthdaycounter/firebase_options.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:birthdaycounter/config/Routes/routes.dart';
 import 'package:birthdaycounter/config/Routes/routes_name.dart';
 import 'package:birthdaycounter/view/splash/splash_screen.dart';
-import 'package:birthdaycounter/config/Routes/routes.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/get_navigation.dart';
-import 'package:birthdaycounter/firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter/material.dart';
+import 'package:birthdaycounter/Services/notification_service.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
@@ -143,7 +61,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-/// A wrapper to handle splash delay + notification payload
+// A wrapper to handle splash delay + notification payload
 class SplashWrapper extends StatefulWidget {
   final String? initialPayload;
   const SplashWrapper({super.key, this.initialPayload});
@@ -170,7 +88,7 @@ class _SplashWrapperState extends State<SplashWrapper> {
           widget.initialPayload!,
         );
         Get.offNamed(AppRoutesName.previewReminder, arguments: reminderData);
-        return;
+        return; 
       } catch (e) {
         print("Error decoding initial payload: $e");
       }
